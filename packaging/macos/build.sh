3d93fec29e1dde19c03dc2c9414969cdbdbd5737
Santosh Bhandari <santoshvandari1@gmail.com>
2025-06-20 20:30:47 +0545

    feat: converted to .exe file

--- packaging/macos/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pyinstaller --onefile --windowed --icon=icon.icns ../src/main.py
-hdiutil create -volname "YT Music Downloader" -srcfolder dist/ -ov -format UDZO yt-music-downloader.dmg
-echo "Build and DMG creation completed."
--- packaging/windows/build.bat
+++ packaging/windows/build.bat
@@ -1,4 +1,11 @@
 @echo off
-pyinstaller --onefile --windowed --icon=icon.ico ..\src\main.py
-echo Build completed.
+echo Cleaning previous builds...
+rmdir /s /q build
+rmdir /s /q dist
+del /q main.spec
+
+echo Building executable with PyInstaller...
+pyinstaller --onefile --windowed --icon=icon.ico ..\..\src\main.py
+
+echo Build complete.
 pause
